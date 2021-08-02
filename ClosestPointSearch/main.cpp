0cb7e2b4a22c20976c5359d2d9f0f2beec1a80e0
gjp <gjp171499@mail.ustc.edu.cn>
2021-08-02 16:51:33 +0800

    添加main测试。

--- ClosestPointSearch/main.cpp
+++ ClosestPointSearch/main.cpp
@@ -14,7 +14,13 @@ int main()
 	// old version include test on both CGAL AABB tree and mine.
 	ClosestPointSearch::AABBTree tree(mesh);
 	std::cout << tree.closest_point(OpenMesh::Vec3d(1, 2, 3)) << std::endl;
+	std::cout << tree.closest_vertex(OpenMesh::Vec3d(1, 2, 3)) << std::endl<< std::endl;
 	std::cout << tree.closest_point(OpenMesh::Vec3d(10, 20, 30)) << std::endl;
+	std::cout << tree.closest_vertex(OpenMesh::Vec3d(10, 20, 30)) << std::endl<< std::endl;
 	std::cout << tree.closest_point(OpenMesh::Vec3d(100, 200, 300)) << std::endl;
+	std::cout << tree.closest_vertex(OpenMesh::Vec3d(100, 200, 300)) << std::endl<< std::endl;
 	std::cout << tree.closest_point(OpenMesh::Vec3d(-10, -20, -30)) << std::endl;
+	auto result = tree.closest_vertex_and_index(OpenMesh::Vec3d(-10, -20, -30));
+	std::cout << result.first << std::endl;
+	std::cout << result.second << " : " << mesh.point(mesh.vertex_handle(result.second)) << std::endl;
 }
