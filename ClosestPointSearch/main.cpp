a6c6d3308e282eeed33c84c5f698435647a5e796
gjp <gjp171499@mail.ustc.edu.cn>
2021-08-05 00:16:15 +0800

    添加测试。

--- ClosestPointSearch/main.cpp
+++ ClosestPointSearch/main.cpp
@@ -13,8 +13,12 @@ int main()
 
 	// old version include test on both CGAL AABB tree and mine.
 	ClosestPointSearch::AABBTree tree(mesh);
-	std::cout << tree.closest_point(OpenMesh::Vec3d(1, 2, 3)) << std::endl;
-	std::cout << tree.closest_point(OpenMesh::Vec3d(10, 20, 30)) << std::endl;
-	std::cout << tree.closest_point(OpenMesh::Vec3d(100, 200, 300)) << std::endl;
-	std::cout << tree.closest_point(OpenMesh::Vec3d(-10, -20, -30)) << std::endl;
+	auto v0 = tree.closest_point_and_face_handle(OpenMesh::Vec3d(1, 2, 3));
+	auto v1 = tree.closest_point_and_face_handle(OpenMesh::Vec3d(10, 20, 30));
+	auto v2 = tree.closest_point_and_face_handle(OpenMesh::Vec3d(100, 200, 300));
+	auto v3 = tree.closest_point_and_face_handle(OpenMesh::Vec3d(-10, -20, -30));
+	std::cout << v0.first << " " << v0.second << std::endl;
+	std::cout << v1.first << " " << v1.second << std::endl;
+	std::cout << v2.first << " " << v2.second << std::endl;
+	std::cout << v3.first << " " << v3.second << std::endl;
 }
